da46147656a365af24c644225d943eb7e86cee9c
jihyukchung <51769097+jihyukchung@users.noreply.github.com>
2019-06-13 00:04:04 -0700

    Add files via upload

---
+++ Assignments/Assignment 4 Tic Tac Toe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private PlayerMove playerMove = new PlayerMove();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Content == null)
+            {
+                button.Content = new PlayerMove(playerMove.PlayerName, ++playerMove.MoveNumber);
+                ChangePlayerName();
+                statusTextBlock.Text = "It's your turn, " + playerMove.PlayerName;             
+            }
+        }
+
+        private void ChangePlayerName()
+        {
+            if (playerMove.PlayerName == "X")
+            {
+                playerMove.PlayerName = "O";
+            }
+            else
+            {
+                playerMove.PlayerName = "X";
+            }
+        }
+    }
+}
---
+++ Assignments/Assignment 4 Tic Tac Toe/TicTacToe/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class PlayerMove : INotifyPropertyChanged
+    {
+        public PlayerMove() : this("X", 0) { }
+        public PlayerMove(string name, int move_number)
+        {
+            this.playerName = name;
+            this.moveNumber = move_number;
+        }
+
+        // INotifyPropertyChanged Member
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private string playerName;
+        public string PlayerName
+        {
+            get { return playerName; }
+            set
+            {
+                playerName = value;
+                Notify("PlayerName");
+            }
+        }
+
+        private int moveNumber;
+        public int MoveNumber
+        {
+            get { return moveNumber; }
+            set { moveNumber = value; }
+        }
+    }
+}
